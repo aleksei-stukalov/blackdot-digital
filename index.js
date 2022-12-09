fc9bbad8d71978bd14be48c3fc3ff30192767bff
Aleksei Stukalov <stukalov.nz@gmail.com>
2022-12-09 15:35:25 +1300

    Create index.js
    Entry for nodeJS

---
+++ index.js
@@ -0,0 +1,41 @@
+// Entry JS file for NodeJS
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    let parsedURL = url.parse(req.url, true);
+    let path = parsedURL.path.replace(/^\/+|\/+$/g, '');
+    if (path === '') {
+        path = 'index.html';
+    }
+    console.log(`Requested path ${path} `);
+
+    let file = __dirname + '/public/' + path;
+    fs.readFile(file, function(err, content) {
+        if (err) {
+            console.log(`File Not Found ${file}`);
+            res.writeHead(404);
+            res.end();
+        } else {
+            console.log(`Returning ${path}`);
+            res.setHeader('X-Content-Type-Options', 'nosniff');
+            switch (path) {
+                case 'index.html':
+                    res.writeHead(200, { 'Content-type': 'text/html' });
+                    break;
+                case 'static/styles.css':
+                    res.writeHead(200, { 'Content-type': 'text/css' });
+                    break;
+                default:
+                    break;
+            }
+            res.end(content);
+        }
+    });
+});
+
+server.listen(8080, 'localhost', () => {
+    console.log('Listening on port 8080')
+})
