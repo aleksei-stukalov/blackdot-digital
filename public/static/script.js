dc826e31b7cb4348068d9ea9f357122ec8ff5d7b
Alek Groove <stukalov.nz@gmail.com>
2022-12-19 11:29:56 +1300

    Changes

--- public/static/script.js
+++ public/static/script.js
@@ -1,7 +1,8 @@
+const d = document
+
 // Listener to check the hights of the vieport
 // change of the css variable to represent vh
 
-console.log('loaded')
 
 //Testing
 // window.addEventListener('resize', () => {
@@ -10,3 +11,30 @@ console.log('loaded')
 //     document.documentElement.style.setProperty('--vh', vh + 'px');
 //     document.querySelector('#hero>h1').innerText = vh
 // })
+
+/*
+// Adding dots on the background of the hero section for desktop version
+const heroSection = d.querySelector('#hero') // Referencing the HTML element
+const heroWidth = heroSection.clientWidth
+const heroHeight = heroSection.clientHeight
+const heroDotSize = Math.floor(heroWidth * 0.05) // The size of the dot is 5% of the width of the viewport
+// We want to place dots side by side the whole width and height of the hero section, and we want them to
+// go over the edge if needed. It is a temprory solution, in the future we want to find a way to scale dots
+// dynamically, so they are alway touching sides of the screen without being cut off.
+const heroDotsHorisonal = Math.floor(heroWidth / heroDotSize) + 1
+const heroDotsVertical = Math.floor(heroHeight / heroDotSize) + 1
+const heroDotsCount = heroDotsHorisonal * heroDotsVertical // We want to create this many dots in a loop
+// We need a container to hold our graphics, with its ID set to blackdot for the cool factor
+const heroDotContainer = d.createElement('div')
+heroDotContainer.id = 'blackdot'
+// Lets fill up the container
+for (let count = 0; count < 0; count++) {
+    const dot = d.createElement('span')
+    dot.classList = 'dot'
+    //heroDotContainer.appendChild(dot)
+}
+//heroSection.appendChild(heroDotContainer)
+
+
+console.log(heroWidth / heroDotSize)
+*/
